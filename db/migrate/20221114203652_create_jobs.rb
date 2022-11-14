1d512fc47386aad93a2a83bba0ad551935eb4ac4
Michael Davis <jrmdd1496@gmail.com>
2022-11-14 14:42:42 -0600

    Created migration for jobs

--- db/migrate/20221114203652_create_jobs.rb
+++ db/migrate/20221114203652_create_jobs.rb
@@ -1,5 +1,9 @@
 class CreateJobs < ActiveRecord::Migration[6.1]
   def change
-    
+    create_table :jobs do |t|
+      t.string :job_title
+      t.string :description
+      t.timestamps
+    end
   end
 end
