59db8b807814b10a72151565abd6d2047fc563aa
Michael Davis <jrmdd1496@gmail.com>
2022-11-17 17:30:25 -0600

    Starting to build routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/Jobs" do
+    Job.all.to_json
+  end
+
+  get "/Categories" do
+    Job.all.to_json
   end
 
 end
